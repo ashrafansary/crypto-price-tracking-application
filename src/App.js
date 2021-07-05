0878058589fb5b191df68ec356deb9f8d6ef031c
ansaryashraf <ansaryashraf@gmail.com>
2021-07-05 17:08:06 +0600

    cryptocurrency finance price tracking application

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import Crypto from './Crypto';
+
+// https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
+
+function App() {
+
+  const [coins, setCoins] = useState([])
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+      .then(res => {
+        setCoins(res.data)
+        console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, [])
+
+  const handleChange = (e) => {
+    setSearch(e.target.value)
+  }
+
+  const filteredCoins = coins.filter(coin => {
+    return coin.name.toLowerCase().includes(search.toLowerCase())
+  })
+
+  return (
+    <div className="crypto-app">
+      <div className="crypto-search">
+        <h1 className="crypto-search-currency">Search a currency</h1>
+        <form>
+          <input type="text" placeholder="Search here" className="crypto-search-input" onChange={handleChange }/>
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return (
+          <Crypto 
+          key={coin.id} 
+          name={coin.name} 
+          image={coin.image}
+          symbol={coin.symbol}
+          marketcap={coin.market_cap}
+          price={coin.current_price}
+          priceChange={coin.price_change_percentage_24h}
+          volume={coin.total_volume}
+          />
+        )
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Crypto.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Crypto.css'
+
+function Crypto({image, name, symbol, price, volume, priceChange, marketcap}) {
+    return (
+        <>
+            <div className="crypto-container">
+                <div className="crypto-row">
+                    <div className="crypto">
+                        <img src={image} alt="crypto-image" />
+                        <h1>{name}</h1>
+                        <p className="crypto-symbol">{symbol}</p>
+                    </div>
+                    <div className="crypto-data">
+                        <p className="crypto-price">{price}</p>
+                        <p className="crypto-volume">{volume.toLocaleString()}</p>
+                        {priceChange < 0 ? 
+                            (<p className="crypto-percent red">{priceChange.toFixed(2)}%</p>)
+                            : (<p className="crypto-percent green">{priceChange.toFixed(2)}%</p>)
+                        }
+                        <p className="crypto-marketcap">Mkt cap : {marketcap.toLocaleString()}</p>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Crypto
